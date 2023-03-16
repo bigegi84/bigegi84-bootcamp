be4647ee341d8ff3404ea9bafe05d0631895f4a3
bigegi84 <gilangpw84@gmail.com>
2023-03-16 13:35:51 +0000

    up

--- api-express-microservice/jurusan/main.js
+++ api-express-microservice/jurusan/main.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('sqlite::memory:')
+
+const main= async()=>{
+    const User = sequelize.define('User', {
+        // Model attributes are defined here
+        firstName: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        lastName: {
+          type: DataTypes.STRING
+          // allowNull defaults to true
+        }
+      }, {
+        // Other model options go here
+      });
+
+
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
